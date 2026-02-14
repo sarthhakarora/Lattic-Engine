6bfa3166485657873c39b54731a584532d3e9b3a
Sarthhak Arora <sarthhaka@gmail.com>
2026-02-15 00:10:41 +0530

    Bug Fix: use luaL_checkinteger for id instead of luaL_checknumber

--- src/luaapi/luaapi.c
+++ src/luaapi/luaapi.c
@@ -146,7 +146,7 @@ int l_world_add_planet(lua_State *L)
     lua_pop(L, 1);
 
     lua_getfield(L, 1, "mass");
-    int mass = luaL_checkinteger(L, -1);
+    int mass = luaL_checknumber(L, -1);
     lua_pop(L, 1);
 
     lua_getfield(L, 1, "has_gravity");
